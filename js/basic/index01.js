966bfc10b0ebc44bc4b41008c8d0fbb6371b4b99
jkiwkoo <jkiwkoo@naver.com>
2023-08-31 15:37:12 +0900

    index01

--- js/basic/index01.js
+++ js/basic/index01.js
@@ -1,1 +1,21 @@
-console.log("welcome");
+const all = {
+  type: 'it',
+  title: 'javascript',
+  'all-time': 300,
+  num: [0, 1, 2],
+  str: {
+    a: 'no01',
+    b: 'no02',
+  },
+};
+
+console.log(all.type); // it
+console.log(all['title']); // javascript
+console.log(all['all-time']); // 300
+
+console.log(all.num[0]); // 0
+console.log(all['num'][0]); // 0
+
+console.log(all.str['a']); // no01
+console.log(all.str.a); // no01
+console.log(all['str'].a); // no01

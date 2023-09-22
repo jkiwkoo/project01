5643a1dea6d692ec9c9d28334b75cca5daa5b69b
jkiwkoo <jkiwkoo@naver.com>
2023-09-22 14:43:16 +0900

    class02

--- js/class/class01.js
+++ js/class/class01.js
@@ -4,20 +4,9 @@ function User(first, last) {
 }
 
 User.prototype.getCall = function () {
-    document.write(`<p>${this.first} : ${this.last}</p>`);
+    document.write(`<p>${this.first} = ${this.last}</p>`);
 };
 
-const myName = [
-    new User('red', 'lee'),
-    new User('blue', 'kim'),
-    new User('pink', 'park'),
-];
+const myName = [new User('red', 'lee'), new User('blue', 'kim'), new User('pink', 'park')];
 
 myName[0].getCall();
-
-function Picture(pic, title, artist, year) {
-    this.pic = pic;
-    this.title = title;
-    this.artist = artist;
-    this.year = year;
-}
--- js/class/class02.js
+++ js/class/class02.js
@@ -4,17 +4,11 @@ class User {
         this.last = last;
     }
     getCall() {
-        document.write(`
-        <p>${this.first} : ${this.last}</p>
-        `);
+        document.write(`<p>${this.first} : ${this.last}</p>`);
     }
 }
 
-const myName = [
-    new User('red', 'lee'),
-    new User('blue', 'kim'),
-    new User('pink', 'park'),
-];
+const myName = [new User('red', 'lee'), new User('blue', 'kim'), new User('pink', 'park')];
 
 for (let i of myName) {
     i.getCall();
